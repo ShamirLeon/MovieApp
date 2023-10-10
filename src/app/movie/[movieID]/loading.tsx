9ea3438cf64a9151bf643f231df05b7db3d5e737
Shamir León <leonshamir1@gmail.com>
2023-10-10 14:17:40 -0500

    Movie deatils page to client component

---
+++ src/app/movie/[movieID]/loading.tsx
@@ -0,0 +1,4 @@
+export default function Loading() {
+    // Or a custom loading skeleton component
+    return <p style={{marginLeft:'220px'}}>'Loading...'</p>
+  }
--- src/app/movie/[movieID]/page.tsx
+++ src/app/movie/[movieID]/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { IMovieDetails, IMovieCredits, IMovieImages, IMovieRecommendations } from "@/Interfaces/Movies.interface";
 
 import Rating from "@/components/Rating/Rating";
@@ -6,135 +8,146 @@ import styles from "@/Styles/movieDetails.module.scss";
 
 import Category from "@/components/Category/Category";
 import AlsoLikeContainer from "@/components/AlsoLike/AlsoLikeContainer";
+import { useEffect, useState } from "react";
+import { optionFecthMovieApi } from "@/API_CONFIG";
 
-function getMovieDetails(id: number) {
-    try {
-        const movieDetails = fetch(`https://api.themoviedb.org/3/movie/${id}`, {
-            method: "GET",
-            headers: {
-                accept: "application/json",
-                Authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN_TMDB_API}`,
-            },
-            cache: 'no-store'
-        }).then(res => res.json());
-        console.log('MovieDetails', movieDetails);
-        return movieDetails;
-    } catch (error) {
-        console.log(error);
-    }
-}
-function getMovieCredits(id: number) {
-    try {
-        const movieCredits = fetch(`https://api.themoviedb.org/3/movie/${id}/credits`, {
-            method: "GET",
-            headers: {
-                accept: "application/json",
-                Authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN_TMDB_API}`,
-            },
-            cache: 'no-store'
-        }).then(res => res.json());
-        return movieCredits;
-    } catch (error) {
-        console.log(error);
-    }
-}
-function getMovieImages(id: number) {
-    try {
-        const movieImages = fetch(`https://api.themoviedb.org/3/movie/${id}/images`, {
-            method: "GET",
-            headers: {
-                accept: "application/json",
-                Authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN_TMDB_API}`,
-            },
-            cache: 'no-store'
-        }).then(res => res.json());
-        return movieImages;
-    } catch (error) {
-        console.log(error);
-    }
-}
-function getMovieRecommendations(id: number) {
-    try {
-        const movieRecommendations = fetch(`https://api.themoviedb.org/3/movie/${id}/recommendations?page=1`, {
-            method: "GET",
-            headers: {
-                accept: "application/json",
-                Authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN_TMDB_API}`,
-            },
-            cache: 'no-store'
-        }).then(res => res.json());
-        return movieRecommendations;
-    } catch (error) {
-        console.log(error);
-    }
-}
 
 const IMG_URL = process.env.NEXT_PUBLIC_IMAGE_TMDB_URL;
 
-export default async function MovieDetailsPage({ params }: any) {
+export default function MovieDetailsPage({ params }: any) {
+
     const { movieID } = params;
-    const movieDetails: IMovieDetails = await getMovieDetails(movieID);
-    const movieCredits: IMovieCredits = await getMovieCredits(movieID);
-    const movieImages: IMovieImages = await getMovieImages(movieID);
-    const movieRecommendations: IMovieRecommendations = await getMovieRecommendations(movieID);
 
-    const { title, overview, backdrop_path, vote_average, genres } = movieDetails;
+    const [movieDetails, setMovieDetails] = useState<IMovieDetails | null>();
+    const [movieCredits, setMovieCredits] = useState<IMovieCredits>();
+    const [movieImages, setMovieImages] = useState<IMovieImages>();
+    const [movieRecommendations, setMovieRecommendations] = useState<IMovieRecommendations>();
+    const [loading, setLoading] = useState(true)
 
-    const filteredBackdrops = movieImages.backdrops.filter((backdrop) => {
+
+    async function getMovieDetails(id: number) {
+        const res = await fetch(`https://api.themoviedb.org/3/movie/${id}`, optionFecthMovieApi)
+
+        if (!res.ok) {
+            throw new Error('Failed to fetch Movie Details')
+        }
+
+        const results = await res.json();
+        setMovieDetails(results);
+        setLoading(false);
+    }
+    async function getMovieCredits(id: number) {
+        try {
+            const res = await fetch(`https://api.themoviedb.org/3/movie/${id}/credits`, optionFecthMovieApi)
+
+            if (!res.ok) {
+                throw new Error('Failed to fetch Movie Credits')
+            }
+
+            const results = await res.json();
+            setMovieCredits(results)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    async function getMovieImages(id: number) {
+        try {
+            const res = await fetch(`https://api.themoviedb.org/3/movie/${id}/images`, optionFecthMovieApi)
+
+            if (!res.ok) {
+                throw new Error('Failed to fetch Movie Images')
+            }
+
+            const results = await res.json();
+            setMovieImages(results)
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    async function getMovieRecommendations(id: number) {
+        try {
+            const res = await fetch(`https://api.themoviedb.org/3/movie/${id}/recommendations?page=1`, optionFecthMovieApi)
+
+            if (!res.ok) {
+                throw new Error('Failed to fetch Movies recommendations')
+            }
+
+            const results = await res.json();
+
+            setMovieRecommendations(results)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getMovieDetails(movieID);
+        getMovieCredits(movieID);
+        getMovieImages(movieID);
+        getMovieRecommendations(movieID);
+    }, [])
+
+    const filteredBackdrops = movieImages && movieImages?.backdrops.filter((backdrop) => {
         return backdrop.aspect_ratio == 1.778;
     })
 
-    const filteredCast = movieCredits.cast.filter((cast) => {
+    const filteredCast = movieCredits && movieCredits?.cast.filter((cast) => {
         return cast.profile_path !== null;
     });
 
     return (
-        <section className={styles.Details_Container}>
-            <div className={styles.Main_Section}>
-                <img className={styles.Main_Section_Img} src={`${IMG_URL}original${backdrop_path}`} alt="" />
-                <div>
-                    <Rating rating={parseFloat(vote_average.toFixed(1))} top={40} />
-                </div>
-                <div className={styles.Texts_Container}>
-                    <h1 className="">{title}</h1>
-                    <div className={styles.Categories}>
-                        {
-                            genres.map(genre => (
-                                <Category key={genre.id} text={genre.name} />
-                            ))
-                        }
-                    </div>
-                    <p>{overview}</p>
-                    <div className={styles.Cast}>
-                        <h2>Cast</h2>
-                        <div className={styles.Cast_Images}>
-                            {
-                                filteredCast.slice(0, 5).map((cast, index) => (
-                                    <picture key={cast.id}>
-                                        <img style={{ marginLeft: `${index == 0 ? '0px' : '-8px'}`, zIndex: `${5 - index}` }} src={`${IMG_URL}original${cast.profile_path!}`} alt={cast.name} />
-                                    </picture>
-                                ))
-                            }
+        <>
+            {!loading && movieDetails && movieCredits && movieImages && movieRecommendations && (
+                <section className={styles.Details_Container}>
+
+                    <div className={styles.Main_Section}>
+                        <img className={styles.Main_Section_Img} src={`${IMG_URL}original${movieDetails?.backdrop_path}`} alt="" />
+                        <div>
+                            <Rating rating={parseFloat(movieDetails?.vote_average.toFixed(1))} top={40} />
+                        </div>
+                        <div className={styles.Texts_Container}>
+                            <h1 className="">{movieDetails?.title}</h1>
+                            <div className={styles.Categories}>
+                                {
+                                    movieDetails?.genres.map(genre => (
+                                        <Category key={genre.id} text={genre.name} />
+                                    ))
+                                }
+                            </div>
+                            <p>{movieDetails?.overview}</p>
+                            <div className={styles.Cast}>
+                                <h2>Cast</h2>
+                                <div className={styles.Cast_Images}>
+                                    {
+                                        filteredCast?.slice(0, 5).map((cast, index) => (
+                                            <picture key={cast.id}>
+                                                <img style={{ marginLeft: `${index == 0 ? '0px' : '-8px'}`, zIndex: `${5 - index}` }} src={`${IMG_URL}original${cast.profile_path!}`} alt={cast.name} />
+                                            </picture>
+                                        ))
+                                    }
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
 
-            <div className={styles.Second_Section}>
-                <div className={styles.Trailer_Section}>
-                    <h2>Trailer</h2>
-                    <div className={styles.Images_Container}>
-                        {
-                            filteredBackdrops.slice(1, 4).map((image, index) => (
-                                <picture key={index}>
-                                    <img src={`${IMG_URL}original${image.file_path}`} alt="" />
-                                </picture>
-                            ))
-                        }
+                    <div className={styles.Second_Section}>
+                        <div className={styles.Trailer_Section}>
+                            <h2>Trailer</h2>
+                            <div className={styles.Images_Container}>
+                                {
+                                    filteredBackdrops?.slice(1, 4).map((image, index) => (
+                                        <picture key={index}>
+                                            <img src={`${IMG_URL}original${image.file_path}`} alt="" />
+                                        </picture>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                        <AlsoLikeContainer movies={movieRecommendations!} />
                     </div>
-                </div>
-                <AlsoLikeContainer movies={movieRecommendations} />
-            </div>
-        </section>
+                </section>
+            )}
+        </>
     )
 }
